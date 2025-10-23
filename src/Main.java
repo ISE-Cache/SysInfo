4c6657f879c281721a6612c3f0fd22fcda412670
donal <25458345@studentmail.ul.ie>
2025-10-23 12:08:35 +0100

    Arrangement of Asci Logo

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,15 @@ public class Main
     {
         int asciIndex;
         //Asci logo
-        String[] ascis = {"\u001B[33m       _.-nnMMMMnn-._       ","    \u001B[33m.d'00000000000000`b.    ",
-                "  \u001B[33m.p'0000\u001B[37m,wWWWWWWw,\u001B[33m0000`q.  ",
-                " \u001B[33m.d'000\u001B[37m,W^'\u001B[33m000000\u001B[37m'^W,\u001B[33m000`b. ",
-                "\u001B[33m.d'00\u001B[37m/WWWWWWWWWW/\u001B[33m00000000`b.",
+        String[] ascis = {"\u001B[33m       _.-nnMMMMnn-._       ","    \u001B[33m.d'000000--000000`b.    ",
+                "  \u001B[33m.p'000/\u001B[37m,wWWWWWWw,\u001B[33m\\000`q.  ",
+                " \u001B[33m.d'00/\u001B[37m,W^'\u001B[33m/0000\\\u001B[37m'^W,\u001B[33m\\00`b. ",
+                "\u001B[33m.d'0/\u001B[37m/WWWWWWWWWW/\u001B[33m/0000000`b.",
                 "\u001B[33m::00000\u001B[37mW:\u001B[33m00000000000000000::",
-                "\u001B[33m`p000\u001B[37m/WWWWWWWWW/\u001B[33m0000000000q'",
-                " \u001B[33m`p0000\u001B[37m'Wv,\u001B[33m000000\u001B[37m,vW'\u001B[33m0000q' ",
-                "  \u001B[33m`b00000\u001B[37m'\\WWWWWW/'\u001B[33m00000d'  ",
-                "    \u001B[33m`q,00000000000000,p'    ","       \u001B[33m'-000;;;;000-'       "};
+                "\u001B[33m`p00/\u001B[37m/WWWWWWWWWW/\u001B[33m/00000000q'",
+                " \u001B[33m`p000\\\u001B[37m'Wv,\u001B[33m\\0000/\u001B[37m,vW'\u001B[33m/000q' ",
+                "  \u001B[33m`b0000\\\u001B[37m'\\WWWWWW/'\u001B[33m/0000d'  ",
+                "    \u001B[33m`q,000000--000000,p'    ","       \u001B[33m'-000;;;;000-'       "};
 
         DataProvider[] dataproviders = new DataProvider[]{};
 
