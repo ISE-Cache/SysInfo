52a139472b3df14e5146750e8c96ef04387275c1
donal <25458345@studentmail.ul.ie>
2025-10-24 13:13:05 +0100

    Reformating of Main - asciPrinting

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -2,17 +2,21 @@ import java.util.ArrayList;
 
 public class Storage implements DataProvider {
     @Override
-    public String getName() {
+    public String getName()
+    {
         return "";
     }
 
     @Override
-    public ArrayList<String> getData() {
+    public ArrayList<String> getData()
+    {
         return null;
     }
 
     @Override
-    public boolean hasData() {
+    public boolean hasData()
+    {
+
         return false;
     }
 }
