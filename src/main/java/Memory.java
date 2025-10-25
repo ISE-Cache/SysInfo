8fc8c97de3f6fc3bfbe63d2db9088e5979015051
donal <25458345@studentmail.ul.ie>
2025-10-25 12:59:15 +0100

    Reformating of Main - asciPrinting

---
+++ src/main/java/Memory.java
@@ -0,0 +1,51 @@
+import oshi.SystemInfo;
+import oshi.hardware.GlobalMemory;
+import oshi.util.FormatUtil;
+import java.util.ArrayList;
+
+public class Memory implements DataProvider
+{
+    private final SystemInfo systemInfo;
+    private final GlobalMemory globalMemory;
+
+    public Memory()
+    {
+        systemInfo = new SystemInfo();
+        globalMemory = systemInfo.getHardware().getMemory();
+    }
+
+    @Override
+    public String getName(){
+        return "Memory";
+    }
+
+    @Override
+    public ArrayList<String> getData()
+    {
+        ArrayList<String> data = new ArrayList<>();
+
+        data.add("Total Memory: " + FormatUtil.formatBytes(globalMemory.getTotal()));
+        data.add("Available Memory: " + FormatUtil.formatBytes(globalMemory.getAvailable()));
+        data.add("Memory Page Size: " + FormatUtil.formatBytes(globalMemory.getPageSize()));
+
+        data.add("Physical Memory: " + globalMemory.getPhysicalMemory());
+        data.add("Physical Memory: " + globalMemory.getPhysicalMemory().getFirst());
+        data.add("Physical Memory: " + globalMemory.getPhysicalMemory().getLast());
+
+        data.add("Virtual Memory: " + globalMemory.getVirtualMemory());
+        data.add("Virtual Memory in use: " + globalMemory.getVirtualMemory().getVirtualInUse());
+        data.add("Virtual Memory maximum: " + globalMemory.getVirtualMemory().getVirtualMax());
+        data.add("Swap Total: " + FormatUtil.formatBytes(globalMemory.getVirtualMemory().getSwapTotal()));
+        data.add("Swap Used: " + FormatUtil.formatBytes(globalMemory.getVirtualMemory().getSwapUsed()));
+        data.add("Swap Pages In: " + FormatUtil.formatBytes(globalMemory.getVirtualMemory().getSwapPagesIn()));
+        data.add("Swap Pages Out: " + FormatUtil.formatBytes(globalMemory.getVirtualMemory().getSwapPagesOut()));
+
+        return data;
+    }
+
+    @Override
+    public boolean hasData()
+    {
+        return globalMemory != null;
+    }
+}
--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,23 +1,35 @@
+import oshi.SystemInfo;
+import oshi.hardware.platform.linux.LinuxHardwareAbstractionLayer;
+
 import java.util.ArrayList;
 
 public class Storage implements DataProvider {
+
+    private final SystemInfo systemInfo;
+
+    public Storage(){
+        systemInfo = new SystemInfo();
+    }
+
     @Override
     public String getName()
     {
-
         return "";
     }
 
     @Override
     public ArrayList<String> getData()
     {
-        return null;
+        ArrayList<String> data = new ArrayList<>();
+
+        //data.add(hardware.getDiskStores().toString());
+
+        return data;
     }
 
     @Override
     public boolean hasData()
     {
-
         return false;
     }
 }
