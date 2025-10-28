767d435fc48b4d15f643630d13332278f0accf7e
donal <25458345@studentmail.ul.ie>
2025-10-28 09:06:34 +0000

    Added Storage and Memory to Main

--- src/main/java/Memory.java
+++ src/main/java/Memory.java
@@ -27,19 +27,19 @@ public class Memory implements DataProvider
         data.add("Total Memory: " + FormatUtil.formatBytes(globalMemory.getTotal()));
         data.add("Available Memory: " + FormatUtil.formatBytes(globalMemory.getAvailable()));
         data.add("Memory Page Size: " + FormatUtil.formatBytes(globalMemory.getPageSize()));
-        data.add("");
+
         data.add("Physical Memory: " + globalMemory.getPhysicalMemory());
         data.add("Physical Memory: " + globalMemory.getPhysicalMemory().getFirst());
         data.add("Physical Memory: " + globalMemory.getPhysicalMemory().getLast());
-        data.add("");
+
         data.add("Virtual Memory: " + globalMemory.getVirtualMemory());
         data.add("Virtual Memory in use: " + globalMemory.getVirtualMemory().getVirtualInUse());
         data.add("Virtual Memory maximum: " + globalMemory.getVirtualMemory().getVirtualMax());
+
         data.add("Swap Total: " + FormatUtil.formatBytes(globalMemory.getVirtualMemory().getSwapTotal()));
         data.add("Swap Used: " + FormatUtil.formatBytes(globalMemory.getVirtualMemory().getSwapUsed()));
         data.add("Swap Pages In: " + FormatUtil.formatBytes(globalMemory.getVirtualMemory().getSwapPagesIn()));
         data.add("Swap Pages Out: " + FormatUtil.formatBytes(globalMemory.getVirtualMemory().getSwapPagesOut()));
-        data.add("");
 
         return data;
     }
