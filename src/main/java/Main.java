65650ec2f0ebfe6f9a24b7c966dda02ddd71266a
CordlessCoder <roman@vm.net.ua>
2025-10-28 13:34:24 +0000

    Add PCI Device count

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     private static final String[] asciiBanner = {"\u001B[33m       _.-nnMMMMnn-._       ", "    \u001B[33m.d'000000--000000`b.    ", "  \u001B[33m.p'000/\u001B[37m,wWWWWWWw,\u001B[33m\\000`q.  ", " \u001B[33m.d'00/\u001B[37m,W^'\u001B[33m/0000\\\u001B[37m'^W,\u001B[33m\\00`b. ", "\u001B[33m.d'0/\u001B[37m/WWWWWWWWWW/\u001B[33m/0000000`b.", "\u001B[33m::00000\u001B[37mW:\u001B[33m00000000000000000::", "\u001B[33m`p00/\u001B[37m/WWWWWWWWWW/\u001B[33m/00000000q'", " \u001B[33m`p000\\\u001B[37m'Wv,\u001B[33m\\0000/\u001B[37m,vW'\u001B[33m/000q' ", "  \u001B[33m`b0000\\\u001B[37m'\\WWWWWW/'\u001B[33m/0000d'  ", "    \u001B[33m`q,000000--000000,p'    ", "       \u001B[33m'-000;;;;000-'       "};
-    private static final DataProvider[] dataProviders = {new CPUProvider(), new GPUProvider(), new IpProvider(), new ProcessCountProvider(), new ShellProvider(), new StorageProvider(), new MemoryProvider()};
+    private static final DataProvider[] dataProviders = {new CPUProvider(), new MemoryProvider(), new GPUProvider(), new PCIProvider(), new IpProvider(), new ProcessCountProvider(), new ShellProvider(), new StorageProvider()};
 
     public static void main(String[] args) {
         // Print an ASCII art banner
